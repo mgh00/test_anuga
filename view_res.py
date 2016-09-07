c6283be67d00d4e5ed5f89401fef250461fb156e
mgh00 <michael.hughes.g@gmail.com>
2016-09-07 22:24:03 +1000

    Update view_res.py

--- view_res.py
+++ view_res.py
@@ -1,10 +1,11 @@
 from netCDF4 import Dataset
 import numpy as np
+import anuga
 
 fid = 'channel3.sww'
 nc = Dataset(fid, mode='r')
-
+b = [1,2,3,4]
 #print nc.variables
 
-#for var in nc.variables:
-#    print var, var.units, var.shape
+for var in nc.variables:
+    print var, var.units, var.shape
